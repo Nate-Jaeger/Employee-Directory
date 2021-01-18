58b0b2c4f84964d0cea1cc5555e1767e33777a75
Nate Jaeger <Nate.RJS21@gmail.com>
2021-01-17 23:54:03 -0600

    Update comments

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const body = document.getElementsByTagName('body')[0];
 //Call to create User Cards with returned data
 const users = fetch(randomUserURL)
 	.then(response => response.json())
+	//Since function is called in this file, UserCard and Modal have access to global variables in this file
 	.then(data =>{createUserCard(data.results);
 					  createModal(data.results)})
 	.catch(error => console.error(error));
